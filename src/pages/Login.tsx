b8a36704021343ee0d606535e8fcd1a20ad6617b
mattpassos <70536543+mattpassos@users.noreply.github.com>
2021-06-16 20:06:06 +0200

    Login typescript page created

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,26 +1,34 @@
-import React, { useState, useEffect } from 'react'
-import { IonPage, IonContent, IonButton, IonInput, IonToolbar } from "@ionic/react";
-
-import ExploreContainer from '../components/ExploreContainer';
+import React, { useState } from 'react'
+import { IonPage, IonContent, IonButton, IonInput, IonToolbar, IonLoading } from "@ionic/react";
+import { Link } from 'react-router-dom';
+import { loginUser } from "../models/firebase"; 
 import Header from "../components/Header";
 import "./Login.css";
+import { toast } from '../toast';
 
 const Login: React.FC = () => {
+
+    const [busy, setBusy] = useState<boolean>(false)
+
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
 
-    function loginUser() {
-        console.log(username, password)
+    async function login() {
+        setBusy(true)
+        const res = await loginUser(username, password)
+        if(res) {
+            toast('You have logged in!')
+        }
+        setBusy(false)
     }
 
   return (
         <IonPage>
             <Header />
+            <IonLoading message="Please wait.." duration={0} isOpen={busy} />
             <IonToolbar className="loginT">Login</IonToolbar>
-            <IonContent>
-            
+            <IonContent className="ion-padding">           
                     <IonInput
-                        type="text" 
                         placeholder="Username" 
                         onIonChange={(e: any) => setUsername(e.target.value)}
                         />
@@ -31,9 +39,8 @@ const Login: React.FC = () => {
                         onIonChange={(e: any) => setPassword(e.target.value)}
                         />
                         <br></br>
-                <IonButton onClick={loginUser}>Login</IonButton>
-            
-            <div className="ion-padding">You don't have an account? Register</div>
+                <IonButton onClick={login}>Login</IonButton>            
+            <p>New here? <Link to="/register">Register</Link></p>
             </IonContent>
         </IonPage>
     );
