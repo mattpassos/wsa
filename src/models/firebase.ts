c8293ea5c2057e8d905552d6a72094ececfd1220
mattpassos <70536543+mattpassos@users.noreply.github.com>
2021-06-16 20:06:20 +0200

    Firebase integration with auth

--- src/models/firebase.ts
+++ src/models/firebase.ts
@@ -1,5 +1,6 @@
 import firebase from "firebase/app"
 import "firebase/auth"
+import { toast } from "../toast"
 
 const app = firebase.initializeApp({
   apiKey: "AIzaSyCTgCbtJaPxWl-7T0aGdHGSlvSOqBUaW2w",
@@ -10,8 +11,33 @@ const app = firebase.initializeApp({
   appId: "1:537748713199:web:c3ee9f9b87a31083d0351a",
 })
 
-export const auth = app.auth()
-export default app
+export async function loginUser(username: string, password: string) {
+  const email = `${username}@whatstreetart.com`
+
+  try {
+    const res = await firebase.auth().signInWithEmailAndPassword(email,password)
+
+    console.log(res)
+    return true
+  } catch(error) {
+    toast(error.message, 4000)
+    return false
+  }
+}
+
+export async function registerUser(username: string, password: string) {
+  const email = `${username}@whatstreetart.com`
+  try {
+    const res = await firebase.auth().createUserWithEmailAndPassword
+
+    (email, password)
+    console.log(res)
+    return true
+  } catch(error) {
+    toast(error.message, 4000)
+    return false
+  }
+}
 
 export enum FirebaseCollection {
   users = "users",
