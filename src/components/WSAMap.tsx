518a6f61d9dee35802c15ae344779ef0b7180874
Matheus Albuquerque <land@ythecombinator.space>
2021-06-22 11:19:27 +0200

    refactor(src/components/WSAMap.tsx): use react-leaflet

--- src/components/WSAMap.tsx
+++ src/components/WSAMap.tsx
@@ -1,12 +1,22 @@
-const WSAMap = L.map('mapid').setView([51.505, -0.09], 13);
+import { FC } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+const position = { lat: 51.505, lng: -0.09 };
+
+const WSAMap: FC = () => {
+  return (
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
 
 export default WSAMap;
