ac8a6fb5acb56e21722b9abc4431027b588e7604
Matheus Albuquerque <land@ythecombinator.space>
2021-06-22 11:32:56 +0200

    fix: proper positioning for the map

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -1,22 +1,21 @@
 import { FC } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "./LeafletMap.css";
 
 const position = { lat: 51.505, lng: -0.09 };
 
-const WSAMap: FC = () => {
+const LeafletMap: FC = () => {
   return (
     <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <Marker position={position}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
+      <div id="leaflet-map"></div>
     </MapContainer>
   );
 };
 
-export default WSAMap;
+export default LeafletMap;
