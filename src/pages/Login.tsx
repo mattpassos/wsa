bcf5dbc7f1a0afacddc105ede7df30ba73230956
mattpassos <70536543+mattpassos@users.noreply.github.com>
2021-07-06 17:47:58 +0200

    CSSing auth pages

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,49 +1,81 @@
-import React, { useState } from 'react'
-import { IonPage, IonContent, IonButton, IonInput, IonToolbar, IonLoading } from "@ionic/react";
-import { Link } from 'react-router-dom';
-import { loginUser } from "../models/firebase"; 
+import React, { useState } from "react";
+import {
+  IonPage,
+  IonContent,
+  IonButton,
+  IonInput,
+  IonText,
+  IonLoading,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+} from "@ionic/react";
+import { Link } from "react-router-dom";
+import { loginUser } from "../models/firebase";
 import Header from "../components/Header";
 import "./Login.css";
-import { toast } from '../toast';
+import { toast } from "../toast";
 
 const Login: React.FC = () => {
+  const [busy, setBusy] = useState<boolean>(false);
 
-    const [busy, setBusy] = useState<boolean>(false)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-
-    async function login() {
-        setBusy(true)
-        const res = await loginUser(username, password)
-        if(res) {
-            toast('You have logged in!')
-        }
-        setBusy(false)
+  async function login() {
+    setBusy(true);
+    const res = await loginUser(username, password);
+    if (res) {
+      toast("You have logged in!");
     }
+    setBusy(false);
+  }
 
   return (
-        <IonPage>
-            <Header />
-            <IonLoading message="Please wait.." duration={0} isOpen={busy} />
-            <IonToolbar className="loginT">Login</IonToolbar>
-            <IonContent className="ion-padding">           
-                    <IonInput
-                        placeholder="Username" 
-                        onIonChange={(e: any) => setUsername(e.target.value)}
-                        />
-                        <br></br>
-                    <IonInput 
-                        type="password"
-                        placeholder="Password" 
-                        onIonChange={(e: any) => setPassword(e.target.value)}
-                        />
-                        <br></br>
-                <IonButton onClick={login}>Login</IonButton>            
-            <p>New here? <Link to="/register">Register</Link></p>
-            </IonContent>
-        </IonPage>
-    );
+    <IonPage>
+      <Header />
+      <IonLoading message="Please wait.." duration={0} isOpen={busy} />
+      <IonContent>
+        <IonCard className="loginCard">
+          <IonCardHeader>
+            <IonText className="t1" color="light">
+              Log in to your Account
+            </IonText>
+            <br></br>
+            <IonText className="t2" color="light">
+              Please enter your Email and Password
+            </IonText>
+          </IonCardHeader>
+          <IonCardSubtitle>
+            <IonInput
+              color="light"
+              className="ionInputLogin"
+              placeholder="Email"
+              onIonChange={(e: any) => setUsername(e.target.value)}
+            />
+            <br></br>
+            <IonInput
+              color="light"
+              className="ionInputLogin"
+              type="password"
+              placeholder="Password"
+              onIonChange={(e: any) => setPassword(e.target.value)}
+            />
+          </IonCardSubtitle>
+        </IonCard>
+        <br></br>
+        <div className="buttonLogin">
+          <IonButton className="button" onClick={login}>
+            Login
+          </IonButton>
+          <IonButton className="button">Cancel</IonButton>
+          <p className="newHere">
+            New here? <Link to="/register">Register</Link>
+          </p>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Login;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,63 +1,95 @@
-import React, { useState } from 'react'
-import { IonPage, IonContent, IonButton, IonInput, IonToolbar, IonLoading } from "@ionic/react";
-import { Link } from 'react-router-dom';
-import { toast } from '../toast';
-import { registerUser } from "../models/firebase"; 
+import React, { useState } from "react";
+import {
+  IonPage,
+  IonContent,
+  IonButton,
+  IonInput,
+  IonCard,
+  IonCardSubtitle,
+  IonText,
+  IonLoading,
+  IonCardHeader,
+} from "@ionic/react";
+import { Link } from "react-router-dom";
+import { toast } from "../toast";
+import { registerUser } from "../models/firebase";
 import Header from "../components/Header";
 import "./Register.css";
 
 const Register: React.FC = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [cpassword, setCPassword] = useState('')
-    const [busy, setBusy] = useState<boolean>(false)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [cpassword, setCPassword] = useState("");
+  const [busy, setBusy] = useState<boolean>(false);
 
-    async function register() {
-        // validation
-        setBusy(true)
-        if(password !== cpassword) {
-            return toast('Passwords do not match')
-        }
-        if(username.trim() === '' || password.trim() === '') {
-            return toast('Username and password are required')
-        }
-
-        const res = await registerUser(username, password)
-        if(res) {
-            toast('You have registered successfully!')
-        }
-        setBusy(false)
+  async function register() {
+    // validation
+    setBusy(true);
+    if (password !== cpassword) {
+      return toast("Passwords do not match");
     }
-        
+    if (username.trim() === "" || password.trim() === "") {
+      return toast("Username and password are required");
+    }
+
+    const res = await registerUser(username, password);
+    if (res) {
+      toast("You have registered successfully!");
+    }
+    setBusy(false);
+  }
 
   return (
-        <IonPage>
-            <Header />
-            <IonLoading message="Registration in progress" duration={0} isOpen={busy} />
-            <IonToolbar className="registerT">Register</IonToolbar>
-            <IonContent className="ion-padding">           
-                    <IonInput
-                        placeholder="Username" 
-                        onIonChange={(e: any) => setUsername(e.target.value)}
-                        />
-                        <br></br>
-                    <IonInput 
-                        type="password"
-                        placeholder="Password" 
-                        onIonChange={(e: any) => setPassword(e.target.value)}
-                        />
-                        <br></br>
-                    <IonInput 
-                        type="password"
-                        placeholder="Password" 
-                        onIonChange={(e: any) => setCPassword(e.target.value)}
-                        />
-                        <br></br>
-                <IonButton onClick={register}>Register</IonButton>            
-            <p>Already have an account? <Link to="/login">Login</Link></p>
-            </IonContent>
-        </IonPage>
-    );
+    <IonPage>
+      <Header />
+      <IonLoading
+        message="Registration in progress"
+        duration={0}
+        isOpen={busy}
+      />
+      <IonContent className="ion-padding">
+        <IonCard className="registerCard">
+          <IonCardHeader>
+            <IonText className="t1" color="light">
+              Create an Account
+            </IonText>
+            <br></br>
+            <IonText className="t2" color="light">
+              Please enter your Email and a Password
+            </IonText>
+          </IonCardHeader>
+          <IonCardSubtitle>
+            <IonInput
+              className="ionInputRegister"
+              placeholder="Username"
+              onIonChange={(e: any) => setUsername(e.target.value)}
+            />
+            <br></br>
+            <IonInput
+              className="ionInputRegister"
+              type="password"
+              placeholder="Password"
+              onIonChange={(e: any) => setPassword(e.target.value)}
+            />
+            <br></br>
+            <IonInput
+              className="ionInputRegister"
+              type="password"
+              placeholder="Confirm Your Password"
+              onIonChange={(e: any) => setCPassword(e.target.value)}
+            />
+          </IonCardSubtitle>
+        </IonCard>
+        <br></br>
+        <div className="buttonRegister">
+          <IonButton onClick={register}>Register</IonButton>
+          <p className="newHere">
+            Already have an account? <Link to="/login">Login</Link>
+          </p>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Register;
